01034ba540498d075083cc3d8998293a2621fdba
Chris Rabl <rabl.christopher@gmail.com>
2021-10-25 16:07:33 -0600

    feat: graph drawing utils

---
+++ src/app/runner/graph.utils.ts
@@ -0,0 +1,111 @@
+// heavily inspired by: https://github.com/jackrusher/jssvggraph/blob/master/graph.js
+
+export interface Vertex {
+  posx: number;
+  posy: number;
+  edges: { [k: string]: Edge };
+  h: number;
+  w: number;
+}
+
+export interface Edge {
+  dest: string;
+}
+
+export class Graph {
+  vertices: { [k: string]: Vertex } = {};
+  forcex: any = {};
+  forcey: any = {};
+
+  repulsion = 200000; // repulsion constant, adjust for wider/narrower spacing
+  spring_length = 500; // base resting length of springs
+  stepsize = 0.0005;
+  iteration = 0;
+  task: any = null;
+  posx = 0;
+  posy = 0;
+
+  createVertex(name: string) {
+    const v: Vertex = {
+      posx: this.posx += 10,
+      posy: this.posy += 10,
+      edges: {},
+      h: 300,
+      w: 200
+    };
+
+    this.vertices[name] = v;
+    this.updateLayout();
+  }
+
+  createEdge(a: any, b: any) {
+    this.vertices[a].edges[b] = { dest : b };
+    this.updateLayout();
+  }
+
+  updateLayout() {
+    for (let iter = 0; iter < 1000; iter++) {
+      for (let i in this.vertices) {
+        this.forcex[i] = 0;
+        this.forcey[i] = 0;
+
+        for (let j in this.vertices) {
+          if (i !== j) {
+            // using rectangle's center, bounding box would be better
+            var deltax = this.vertices[j].posx - this.vertices[i].posx;
+            var deltay = this.vertices[j].posy - this.vertices[i].posy;
+            var d2 = deltax * deltax + deltay * deltay;
+
+            // add some jitter if distance^2 is very small
+            if (d2 < 0.01) {
+              deltax = 0.1 * Math.random() + 0.1;
+              deltay = 0.1 * Math.random() + 0.1;
+              var d2 = deltax * deltax + deltay * deltay;
+            }
+
+            // Coulomb's law -- repulsion varies inversely with square of distance
+            this.forcex[i] -= (this.repulsion / d2) * deltax;
+            this.forcey[i] -= (this.repulsion / d2) * deltay;
+
+            // spring force along edges, follows Hooke's law
+            if (this.vertices[i].edges[j]) {
+              var distance = Math.sqrt(d2);
+              this.forcex[i] += (distance - this.spring_length) * deltax;
+              this.forcey[i] += (distance - this.spring_length) * deltay;
+            }
+          }
+        }
+      }
+
+      for (let i in this.vertices) {
+        // update rectangles
+        this.vertices[i].posx += this.forcex[i] * this.stepsize;
+        this.vertices[i].posy += this.forcey[i] * this.stepsize;
+      }
+    }
+
+    // Once iterations have been completed, move everything to (0,0) coordinate system
+    let minPosX = 0;
+    let minPosY = 0;
+    for (let i in this.vertices) {
+      const { posx, posy } = this.vertices[i];
+      if (posx < minPosX) {
+        minPosX = posx;
+      }
+
+      if (posy < minPosY) {
+        minPosY = posy;
+      }
+    }
+
+    for (let i in this.vertices) {
+      if (minPosX < 0) {
+        this.vertices[i].posx += Math.abs(minPosX);
+      }
+
+      if (minPosY < 0) {
+        this.vertices[i].posy += Math.abs(minPosY);
+      }
+    }
+  }
+}
--- src/app/runner/runner.component.ts
+++ src/app/runner/runner.component.ts
@@ -1,13 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { BbmlItemType, BbmlSyntaxTree } from '../shared/ast.model';
+import { Network, parseDOTNetwork } from 'vis-network/standalone';
+import { Graph, Vertex } from './graph.utils';
+
+const dotString = `digraph dbml {
+  rankdir=LR;
+  graph [fontname="helvetica", fontsize=32, fontcolor="#29235c", bgcolor="transparent"];
+  node [penwidth=0, margin=0, fontname="helvetica", fontsize=32, fontcolor="#29235c"];
+  edge [fontname="helvetica", fontsize=32, fontcolor="#29235c", color="#29235c"];
+
+}`;
 
 @Component({
   selector: 'bb-runner',
   template: `
     <div class="runner">
-      <ng-container *ngFor="let item of ast">
-        <bb-screen [screen]="item" *ngIf="item.type === BbmlItemType.Screen"></bb-screen>
-      </ng-container>
+      
+
+      <div class="graph-container" #graphContainer>
+
+        <div *ngFor="let v of vertices" [ngStyle]="{ position: 'absolute', top: v.posy + 'px', left: v.posx + 'px', border: '1px solid red', width: '200px', height: '300px' }">
+          <ng-container *ngFor="let item of ast">
+            <bb-screen [screen]="item" *ngIf="item.type === BbmlItemType.Screen && item.name === v.name"></bb-screen>
+          </ng-container>
+        </div>
+      </div>
     </div>
   `,
   styles: [`
@@ -19,9 +36,82 @@ import { BbmlItemType, BbmlSyntaxTree } from '../shared/ast.model';
     .runner > * {
       margin: 8px;
     }
+
+    .graph-container {
+      height: 100vh;
+      width: 100vw;
+      border: 1px solid red;
+      position: relative;
+      overflow: auto;
+    }
   `]
 })
 export class RunnerComponent {
   @Input() ast?: BbmlSyntaxTree;
   public BbmlItemType = BbmlItemType;
+  public vertices: any[] = [];
+
+  @ViewChild('graphContainer') graphContainer?: ElementRef;
+ 
+  ngAfterViewInit() {    
+    this.render();
+  }
+
+  ngOnChanges() {
+    this.render();
+  }
+
+  render() {
+    const g = new Graph();
+    g.createVertex('Profile');
+    g.createVertex('Main');
+    g.createVertex('Login');
+    g.createEdge('Main', 'Profile');
+    g.createEdge('Login', 'Main');
+
+    const vertices: any[] = [];
+    for (let v in g.vertices) {
+      vertices.push({
+        ...g.vertices[v],
+        name: v
+      });
+    }
+
+    this.vertices = vertices;
+
+
+    var parsedData = parseDOTNetwork(dotString);
+
+
+
+
+    // const svg = vizRenderStringSync(dotString, {
+    //   engine: 'dot',
+    //   format: 'svg',
+    // });
+
+    // console.log(svg)
+
+    var data = {
+      nodes: parsedData.nodes,
+      edges: parsedData.edges
+    }
+
+    var options = parsedData.options;
+
+    // you can extend the options like a normal JSON variable:
+    options.nodes = {
+      color: 'red'
+    }
+
+
+    // // create a network
+    // var container = this.graphContainer?.nativeElement;
+
+    // // initialize your network!
+    // if (container) {
+    //   var network = new Network(container, data, options);
+    //   console.log(container)
+    // }
+  }
 }
