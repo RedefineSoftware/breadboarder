206354d06acde3a5fab0df89a313da1ee37a3d66
Chris Rabl <rabl.christopher@gmail.com>
2021-10-24 20:32:34 -0600

    feat: add Monaco editor, BBML syntax highlighting, parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,71 @@
 import { Component } from '@angular/core';
 
+// @ts-ignore
+import bbml from './bbml.js';
+
 @Component({
   selector: 'bb-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <div class="container">
+      <div class="code-pane">
+        <ngx-monaco-editor style="height: 100%" [options]="editorOptions" [(ngModel)]="code" (ngModelChange)="parse(code)"></ngx-monaco-editor>
+      </div>
+
+      <div class="breadboard-pane">
+        {{ ast | json }}
+      </div>
+
+      <div class="run-pane">
+      </div>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
   `,
-  styles: []
+  styles: [`
+    .container {
+      display: flex;
+      height: 100vh;
+      width: 100vw;
+    }
+
+    .code-pane, .breadboard-pane, .run-pane {
+      flex: 1;
+    }
+
+    .code-pane {
+      background: #1e1e1e;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'breadboarder';
+  editorOptions = {
+    theme: 'vs-dark',
+    language: 'bbml',
+    minimap: {
+      enabled: false
+    }
+  };
+
+  code = `screen Login {
+  field "Username: "
+  field "Password: "
+  button "Log In" -> Main
+}
+
+screen Main {
+  button "Go to Profile" -> Profile
+  button "Log Out" -> Login
+}`
+
+  ast = [];
+
+  ngOnInit() {
+    this.parse(this.code);
+  }
+
+  parse(code: string) {
+    try {
+      this.ast = bbml.parse(code);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,101 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+declare const monaco: any;
+const monacoConfig: NgxMonacoEditorConfig = {
+  onMonacoLoad() {
+    monaco.languages.register({ id: 'bbml' });
+
+    // Register a tokens provider for BBML
+    monaco.languages.setMonarchTokensProvider('bbml', {
+      keywords: [
+        'if', 'else', 'goto'
+      ],
+
+      typeKeywords: [
+        'screen', 'component', 'button', 'field', 'label'
+      ],
+
+      operators: [
+        '->'
+      ],
+
+      // we include these common regular expressions
+      symbols: /[=><!~?:&|+\-*\/\^%]+/,
+
+      // The main tokenizer for our languages
+      tokenizer: {
+        root: [
+          // identifiers and keywords
+          [/[a-z_$][\w$]*/, {
+            cases: {
+              '@typeKeywords': 'keyword',
+              '@keywords': 'keyword',
+              '@default': 'identifier'
+            }
+          }],
+          [/[A-Z][\w\$]*/, 'type.identifier'],  // to show class names nicely
+
+          // whitespace
+          { include: '@whitespace' },
+
+          // delimiters and operators
+          [/@symbols/, {
+            cases: {
+              '@operators': 'operator',
+              '@default': ''
+            }
+          }],
+
+          // @ annotations.
+          // As an example, we emit a debugging log message on these tokens.
+          // Note: message are supressed during the first load -- change some lines to see them.
+          [/@\s*[a-zA-Z_\$][\w\$]*/, { token: 'annotation', log: 'annotation token: $0' }],
+
+          // numbers
+          [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+          [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+          [/\d+/, 'number'],
+
+          // delimiter: after number because of .\d floats
+          [/[;,.]/, 'delimiter'],
+
+          // strings
+          [/"([^"\\]|\\.)*$/, 'string.invalid'],  // non-teminated string
+          [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+
+          // characters
+          [/'[^\\']'/, 'string'],
+          [/'/, 'string.invalid']
+        ],
+
+        comment: [
+          [/[^\/*]+/, 'comment'],
+          [/\/\*/, 'comment', '@push'],    // nested comment
+          ["\\*/", 'comment', '@pop'],
+          [/[\/*]/, 'comment']
+        ],
+
+        string: [
+          [/[^\\"]+/, 'string'],
+          [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }]
+        ],
+
+        whitespace: [
+          [/[ \t\r\n]+/, 'white'],
+          [/\/\*/, 'comment', '@comment'],
+          [/\/\/.*$/, 'comment'],
+        ]
+      },
+
+    });
+  },
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +103,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MonacoEditorModule.forRoot(monacoConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
