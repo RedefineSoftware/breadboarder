5d1259ee4b6d29d53dc67a6f2b174f8c0e563a41
Chris Rabl <rabl.christopher@gmail.com>
2021-10-24 23:19:09 -0600

    feat: screen items background and border

--- src/app/runner/field.component.ts
+++ src/app/runner/field.component.ts
@@ -4,8 +4,27 @@ import { BbmlField } from '../shared/ast.model';
 @Component({
   selector: 'bb-field',
   template: `
-    {{ field?.label }} <input type="text" />
-  `
+    <div class="field">
+      <label>{{ field?.label }}</label>
+      <input type="text" />
+    </div>
+  `,
+  styles: [`
+    .field { 
+      display: flex; 
+      flex-direction: column;
+    }
+
+    .field label {
+      font-size: 0.6em;
+      margin: 2px;
+    }
+
+    .field input {
+      width: 100%;
+      box-sizing: border-box;
+    }
+  `]
 })
 export class FieldComponent {
   @Input() field?: BbmlField;
--- src/app/runner/screen-items.component.ts
+++ src/app/runner/screen-items.component.ts
@@ -4,14 +4,30 @@ import { BbmlItemType, BbmlScreenItem } from '../shared/ast.model';
 @Component({
   selector: 'bb-screen-items',
   template: `
-    <ng-container *ngFor="let item of items">
-      <bb-field [field]="item" *ngIf="item.type === BbmlItemType.Field"></bb-field>
-      <bb-button [button]="item" *ngIf="item.type === BbmlItemType.Button"></bb-button>
-      <bb-label [label]="item" *ngIf="item.type === BbmlItemType.Label"></bb-label>
-      <bb-component [component]="item" *ngIf="item.type === BbmlItemType.Component"></bb-component>
-      <bb-conditional [conditional]="item" *ngIf="item.type === BbmlItemType.Conditional"></bb-conditional>
-    </ng-container>
-  `
+    <div class="screen-items">
+      <ng-container *ngFor="let item of items">
+        <bb-field [field]="item" *ngIf="item.type === BbmlItemType.Field"></bb-field>
+        <bb-button [button]="item" *ngIf="item.type === BbmlItemType.Button"></bb-button>
+        <bb-label [label]="item" *ngIf="item.type === BbmlItemType.Label"></bb-label>
+        <bb-component [component]="item" *ngIf="item.type === BbmlItemType.Component"></bb-component>
+        <bb-conditional [conditional]="item" *ngIf="item.type === BbmlItemType.Conditional"></bb-conditional>
+      </ng-container>
+    </div>
+  `,
+  styles: [`
+    .screen-items {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .screen-items > * {
+      margin-bottom: 8px;
+      border: 1px solid #ddd;
+      padding: 4px;
+      border-radius: 4px;
+      background: #efefef;
+    }
+  `]
 })
 export class ScreenItemsComponent {
   @Input() items?: BbmlScreenItem[] = [];
