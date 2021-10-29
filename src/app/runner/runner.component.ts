28cd0ce7046da3cc15a4ab224e851c0e85f78bad
Chris Rabl <rabl.christopher@gmail.com>
2021-10-28 21:13:17 -0600

    feat: make edge rendering work with some insanity

--- src/app/runner/runner.component.ts
+++ src/app/runner/runner.component.ts
@@ -3,29 +3,113 @@ import { BbmlComment, BbmlItemType, BbmlScreen, BbmlSyntaxTree } from '../shared
 import ELK from 'elkjs/lib/elk.bundled.js'
 import { findOutgoingNodes } from '../shared/ast.utils';
 import { v4 as uuidV4 } from 'uuid';
+import { curveBundle, line } from 'd3-shape';
+
+function getBezierCenter({
+  sourceX,
+  sourceY,
+  targetX,
+  targetY
+}: any): [number, number, number, number] {
+  const xOffset = Math.abs(targetX - sourceX) / 2;
+  const centerX = targetX < sourceX ? targetX + xOffset : targetX - xOffset;
+
+  const yOffset = Math.abs(targetY - sourceY) / 2;
+  const centerY = targetY < sourceY ? targetY + yOffset : targetY - yOffset;
+
+  return [centerX, centerY, xOffset, yOffset];
+}
+
+/**
+ * Path helper utils.
+ * Ref: https://github.com/wbkd/react-flow/blob/main/src/components/Edges/BezierEdge.tsx#L19
+ */
+export function getBezierPath({
+  sourceX,
+  sourceY,
+  sourcePosition = 'bottom',
+  targetX,
+  targetY,
+  targetPosition = 'top'
+}: any): string {
+  const leftAndRight = ['left', 'right'];
+  const [centerX, centerY] = getBezierCenter({
+    sourceX,
+    sourceY,
+    targetX,
+    targetY
+  });
+
+  let path = `M${sourceX},${sourceY} C${sourceX},${centerY} ${targetX},${centerY} ${targetX},${targetY}`;
+
+  if (
+    leftAndRight.includes(sourcePosition) &&
+    leftAndRight.includes(targetPosition)
+  ) {
+    path = `M${sourceX},${sourceY} C${centerX},${sourceY} ${centerX},${targetY} ${targetX},${targetY}`;
+  } else if (leftAndRight.includes(targetPosition)) {
+    path = `M${sourceX},${sourceY} C${sourceX},${targetY} ${sourceX},${targetY} ${targetX},${targetY}`;
+  } else if (leftAndRight.includes(sourcePosition)) {
+    path = `M${sourceX},${sourceY} C${targetX},${sourceY} ${targetX},${sourceY} ${targetX},${targetY}`;
+  }
+
+  return path;
+}
+
+function computeSVGPath(sections: any[]) {
+  if (!sections?.length) {
+    return null;
+  }
+
+  // Handle bend points that elk gives
+  // us separately from drag points
+  if (sections[0].bendPoints) {
+    const points: any[] = sections
+      ? [
+        sections[0].startPoint,
+        ...(sections[0].bendPoints || ([] as any)),
+        sections[0].endPoint
+      ]
+      : [];
+
+    const pathFn = line()
+      .x((d: any) => d.x)
+      .y((d: any) => d.y);
+
+    return pathFn(points);
+  } else {
+    return getBezierPath({
+      sourceX: sections[0].startPoint.x,
+      sourceY: sections[0].startPoint.y,
+      targetX: sections[0].endPoint.x,
+      targetY: sections[0].endPoint.y
+    });
+  }
+}
 
 @Component({
   selector: 'bb-runner',
   template: `
     <div class="runner">
-      <div class="graph-container">
-        <!-- <svg class="edges">
+      <div class="graph-container" [ngStyle]="{ width: graphSize.width + 'px', height: graphSize.height + 'px' }">
+        <svg class="edges" [ngStyle]="{ width: graphSize.width + 'px', height: graphSize.height + 'px' }">
           <defs>
-            <marker id="arrowhead" markerWidth="10" markerHeight="7" 
-            refX="0" refY="3.5" orient="auto">
-              <polygon points="0 0, 10 3.5, 0 7" />
+            <marker id="arrowhead" markerWidth="5" markerHeight="6" 
+            refX="0" refY="3" orient="auto">
+              <polygon points="0 0, 5 3, 0 6" />
             </marker>
           </defs>
-          <line *ngFor="let edge of edges"
-            [attr.x1]="edge.x1" [attr.y1]="edge.y1" [attr.x2]="edge.x2" [attr.y2]="edge.y2" 
-            stroke="#000" stroke-width="2" marker-end="url(#arrowhead)"></line>
-        </svg> -->
+          <path *ngFor="let edge of edges" [attr.d]="edge.path"
+            stroke="#999" stroke-width="1" fill="transparent" marker-end="url(#arrowhead)"></path>
+        </svg>
 
-        <div *ngFor="let v of vertices" [ngStyle]="{ position: 'absolute', top: v.y + 'px', left: v.x + 'px', width: '200px', height: '320px' }">
-          <ng-container *ngFor="let item of ast">
-            <bb-screen [screen]="item" *ngIf="item.type === BbmlItemType.Screen && item.name === v.id"></bb-screen>
-          </ng-container>
-        </div>
+        <ng-container *ngFor="let v of vertices">
+          <div [ngStyle]="{ position: 'absolute', top: v.y + 'px', left: v.x + 'px', width: '220px', height: '340px', 'padding': '10px', 'box-sizing': 'border-box', display: 'flex', 'justify-content': 'center' }">
+            <ng-container *ngFor="let item of ast">
+              <bb-screen [screen]="item" *ngIf="item.type === BbmlItemType.Screen && item.name === v.id"></bb-screen>
+            </ng-container>
+          </div>
+        </ng-container>
       </div>
     </div>
   `,
@@ -33,11 +117,10 @@ import { v4 as uuidV4 } from 'uuid';
     .runner {
       display: flex;
       flex: 1;
+      overflow: auto;
     }
 
     .edges {
-      width: 100%;
-      height: 100%;
       position: relative;
     }
 
@@ -46,10 +129,7 @@ import { v4 as uuidV4 } from 'uuid';
     }
 
     .graph-container {
-      height: 100vh;
-      width: 100vw;
       position: relative;
-      overflow: auto;
     }
   `]
 })
@@ -58,6 +138,7 @@ export class RunnerComponent {
   public BbmlItemType = BbmlItemType;
   public vertices: any[] = [];
   public edges: any[] = [];
+  public graphSize: { width: number; height: number } = {width: 0, height: 0};
  
   ngAfterViewInit() {    
     this.render();
@@ -75,8 +156,26 @@ export class RunnerComponent {
     const children = screens?.map((screen: BbmlScreen) => {
       return {
         id: screen.name,
-        width: 200,
-        height: 320
+        width: 220,
+        height: 340,
+        ports: [{
+          id: screen.name + '-from',
+          properties: {
+            'port.index': '0',
+            'port.side': 'WEST',
+            'port.alignment': 'CENTER'
+          }
+        }, {
+          id: screen.name + '-to',
+          properties: {
+            'port.index': '1',
+            'port.side': 'EAST',
+            'port.alignment': 'CENTER'
+          }
+        }],
+        properties: {
+          portConstraints: 'FIXED_ORDER'
+        }
       }
     });
 
@@ -88,36 +187,52 @@ export class RunnerComponent {
         .map((target: string)=> {
           return {
             id: uuidV4(),
-            sources: [screen.name],
-            targets: [target]
+            source: screen.name,
+            target: target,
+            fromPort: screen.name + '-from',
+            toPort: target + '-to'
           }
         });
     });
 
     const graph = {
       id: "root",
-      layoutOptions: { 'elk.algorithm': 'layered' },
+      layoutOptions: { 
+        'elk.algorithm': 'layered',
+        'elk.direction': 'RIGHT',
+        'org.eclipse.elk.layered.layering.strategy': 'INTERACTIVE',
+        'org.eclipse.elk.edgeRouting': 'ORTHOGONAL',
+        'elk.layered.unnecessaryBendpoints': 'true',
+        'elk.layered.spacing.edgeNodeBetweenLayers': '50',
+        'org.eclipse.elk.layered.nodePlacement.bk.fixedAlignment': 'BALANCED',
+        'org.eclipse.elk.layered.cycleBreaking.strategy': 'DEPTH_FIRST',
+        'org.eclipse.elk.insideSelfLoops.activate': 'true',
+        portConstraints: 'FIXED_ORDER',
+        separateConnectedComponents: 'true',
+        'spacing.componentComponent': '70',
+        spacing: '75',
+        'spacing.nodeNodeBetweenLayers': '70',
+        'elk.padding': '[left=10, top=12, right=10, bottom=12]'
+      },
       children,
       edges
-    }
+    };
 
     elk.layout(graph)
       .then((g) => {
+        this.graphSize = {
+          width: g.width || 0,
+          height: g.height || 0
+        };
+
         this.vertices = g.children || [];
 
-        const edges_with_geometry = edges.map((e) => {
-          const [ source_name ] = e.sources;
-          const [ source_vertex ] = this.vertices.filter((v) => v.id === source_name);
-
-          const [ target_name ] = e.targets;
-          const [ target_vertex ] = this.vertices.filter((v) => v.id === target_name);
+        const edges_with_geometry = edges.map((e: any) => {
+          console.log(e)
 
           return {
             ...e,
-            x1: source_vertex.x + 200,
-            y1: source_vertex.y + 160,
-            x2: target_vertex.x,
-            y2: target_vertex.y
+            path: computeSVGPath(e.sections)
           };
         });
 
